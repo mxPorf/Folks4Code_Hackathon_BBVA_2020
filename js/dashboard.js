f68d18f60b914ab2f254ec6dc8a93154ff17fc73
mxPorf <porfirioBasaldua@gmail.com>
2020-10-18 02:52:20 -0500

    Integrated bot to the application

--- js/dashboard.js
+++ js/dashboard.js
@@ -12,7 +12,8 @@ $('#firstPage').click( () => {
   //link to conversa platform
   //window.open('https://conversabbva.btgapps.com/conversa/faceme?instanceId=31790727-8632-383e-b884-5eb2b9e74ddd&langCode=es_ES');
   //link to Uneeq conversational bot
-  window.open('https://creator.uneeq.io/personas/try/d67d22b3-3da7-4079-850f-f5199532ae27');
+  //window.open('https://creator.uneeq.io/personas/try/d67d22b3-3da7-4079-850f-f5199532ae27');
+  window.open('conversation_bot.html');
 });
 
 $('#secondPage').click( () => {
